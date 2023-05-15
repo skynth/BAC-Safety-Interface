b7d568cdb37d2be2cf38f60d3540453803819667
Anthony Sky Ng-Thow-Hing <skyngthowhing@gmail.com>
2023-05-15 10:53:48 -0400

    Initial Commit

---
+++ BAC-Tracker/BAC_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAC_TrackerApp.swift
+//  BAC-Tracker
+//
+//  Created by Anthony Sky Ng-Thow-Hing on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BAC_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BAC-Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BAC-Tracker
+//
+//  Created by Anthony Sky Ng-Thow-Hing on 5/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
